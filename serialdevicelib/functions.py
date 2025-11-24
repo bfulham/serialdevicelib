214a36aeb7c595568403c7f04e2ec4eac199ec9b
bobisaperson1 <bfulham@bradyfulham.com>
2025-11-24 15:55:15 +1030

    0.2.1

--- serialdevicelib/functions.py
+++ serialdevicelib/functions.py
@@ -1,4 +1,5 @@
 import logging
+from pprint import pformat
 
 log = logging.getLogger("serialdevicelib")
 
@@ -12,16 +13,16 @@ def Generate_Checksum(data: str):
         sum = hex(int(sum, 16) ^ int(i, 16))
     return str(sum).replace("0x", "").upper().zfill(2)
 
-def Generate_Command(Control_ID: str, Group: str, Command: str, bible, Data: tuple[str, ...]):
+def Generate_Command(Control_ID: str, Group: str, Command: str, bible, Data: tuple[int, ...]):
     temp_command = ""
     temp_command += str(len(Data) + 5).zfill(2)
     temp_command += Control_ID
     temp_command += Group
     temp_command += Command
     for i in Data:
-        temp_command += i
+        temp_command += f"{i:02x}"
     Full_command = temp_command + Generate_Checksum(temp_command)
-    log.debug("Command: %s", Full_command)
+    log.info("Command: %s", Full_command)
     Decode_Hex(Full_command, bible, "command")
     return Full_command
 
@@ -37,10 +38,10 @@ def Decode_Hex(Hex, bible, Hex_type="response"):
     command = data[0]
     checksum = b[-1]
     if  int(Generate_Checksum(Hex[:-2]), 16) == int(checksum, 16):
-        log.info("Checksum OK")
+        log.debug("Checksum OK")
     else:
         log.warning("Checksum failed")
-    log.debug("Command: %s", bible[command]['name'])
+    log.info("Command: %s", bible[command]['name'])
     response = b[4:-1]
     if Hex_type == "command":
         return None
@@ -87,8 +88,10 @@ def Decode_Hex(Hex, bible, Hex_type="response"):
                     p[i] = int(p[i], 16)
             to_return[key] = p
     if len(to_return) == 1:
+        log.info("Data: %s", list(to_return.values())[0])
         return list(to_return.values())[0]
     else:
+        log.info("Data: %s", pformat(to_return))
         return to_return
 
 def check_response(control_ID, group_ID, response):
